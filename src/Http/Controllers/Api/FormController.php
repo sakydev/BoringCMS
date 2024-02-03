208967cc259ae83fca310c90a846b97037a765c0
Saqib Razzaq <saqib.razzaq@plana.earth>
2024-02-03 14:50:26 +0100

    simple model example

--- src/Http/Controllers/Api/FormController.php
+++ src/Http/Controllers/Api/FormController.php
@@ -3,8 +3,16 @@
 namespace Sakydev\Boring\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+use Sakydev\Boring\Models\Form;
+use Symfony\Component\HttpFoundation\Response;
 
 class FormController extends Controller
 {
-    //
+    public function index(): JsonResponse {
+        $forms = new Form();
+        $results = $forms->all();
+
+        return new JsonResponse($results, Response::HTTP_OK);
+    }
 }
