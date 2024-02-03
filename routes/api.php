d5e9db2553b545fdb17c4e2ae3cd4791daf05185
Saqib Razzaq <saqib.razzaq@plana.earth>
2024-02-03 14:50:16 +0100

    group api routes

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use Sakydev\Boring\Http\Controllers\Api\StatusController;
+use Sakydev\Boring\Http\Controllers\Api\FormController;
 
-Route::get('/status/{item}', [StatusController::class, 'status']);
+Route::prefix('api')->group(function() {
+    Route::get('/status/{item}', [StatusController::class, 'status']);
+
+    // Forms
+    Route::prefix('/forms')->group(function() {
+        Route::get('/', [FormController::class, 'index']);
+    });
+});
