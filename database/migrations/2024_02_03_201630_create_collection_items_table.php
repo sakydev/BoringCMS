c532b2de5c02127c2cf1874699521f566564e071
Saqib Razzaq <saqib.razzaq@plana.earth>
2024-02-04 01:36:30 +0100

    add `collections` table

---
+++ database/migrations/2024_02_03_201630_create_collection_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('collection_items', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('collection_id');
+            $table->string('name');
+            $table->string('slug');
+            $table->timestamps();
+
+            $table->foreign('collection_id')
+                ->references('id')
+                ->on('collections')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('collection_items');
+    }
+};
