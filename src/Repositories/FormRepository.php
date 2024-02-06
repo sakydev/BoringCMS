8973ae0d13583915c6d39e2200334b9ea7f629ed
Saqib Razzaq <saqib.razzaq@plana.earth>
2024-02-06 17:57:36 +0100

    add show form endpoint

--- src/Repositories/FormRepository.php
+++ src/Repositories/FormRepository.php
@@ -9,6 +9,11 @@ class FormRepository
     public function getById(int $formId): ?Form {
         return (new Form())->find($formId);
     }
+
+    public function getBySlug(string $slug): ?Form {
+        return (new Form())->where('slug', $slug)->first();
+    }
+
     public function store(array $formData): Form {
         $form = new Form();
 
