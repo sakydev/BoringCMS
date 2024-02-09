a49cbd9058a9dd90dc27692919510aee71365148
Saqib Razzaq <saqib.razzaq@plana.earth>
2024-02-09 18:54:54 +0100

    add /me endpoint

--- routes/api-group/account.php
+++ routes/api-group/account.php
@@ -3,8 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use Sakydev\Boring\Http\Controllers\Api\RegisterController;
 use Sakydev\Boring\Http\Controllers\Api\LoginController;
+use Sakydev\Boring\Http\Controllers\Api\UserController;
 
 Route::prefix('/account')->group(function() {
-    Route::post('/register', [RegisterController::class, 'store']);
-    Route::post('/login', [LoginController::class, 'login']);
+    Route::post('/register', [RegisterController::class, 'store'])->name('account.register');
+    Route::post('/login', [LoginController::class, 'login'])->name('account.login');
+
+    Route::middleware(['auth:sanctum'])->group(function() {
+        Route::get('/me', [UserController::class, 'me'])->name('account.me');
+    });
 });
