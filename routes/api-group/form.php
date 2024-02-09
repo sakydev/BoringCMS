d15429e814a8febe5ac01a5366f72f146ecc6d5e
Saqib Razzaq <saqib.razzaq@plana.earth>
2024-02-09 18:59:47 +0100

    protect form endpoints

--- routes/api-group/form.php
+++ routes/api-group/form.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Sakydev\Boring\Http\Controllers\Api\FormController;
 
-Route::prefix('/forms')->group(function() {
+Route::prefix('/forms')->middleware(['auth:sanctum'])->group(function() {
     Route::get('/', [FormController::class, 'index'])->name('forms.index');
     Route::Post('/', [FormController::class, 'store'])->name('forms.store');
 });
