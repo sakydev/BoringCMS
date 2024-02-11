87920b05383c872eed508043d38dff52ab990ed9
Saqib Razzaq <saqib.razzaq@plana.earth>
2024-02-11 11:11:08 +0100

    change tables to include uuid

--- migrations/2024_02_03_114641_create_forms_table.php
+++ migrations/2024_02_03_114641_create_forms_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -13,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('forms', function (Blueprint $table) {
             $table->id();
-            $table->uuid()->primary();
+            $table->uuid()->default(DB::raw('uuid_generate_v4()'))->index();
             $table->unsignedBigInteger('user_id');
             $table->string('name');
             $table->string('slug');
