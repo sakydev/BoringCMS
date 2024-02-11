c01825f8072e1069e1654537f6b814755f1a5ee8
Saqib Razzaq <saqib.razzaq@plana.earth>
2024-02-11 10:37:49 +0100

    add Field resource

---
+++ src/Resources/Api/FieldResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Sakydev\Boring\Resources\Api;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class FieldResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'collection_id' => $this->collection_id,
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'validation' => $this->validation,
+            'condition' => $this->condition,
+            'is_required' => $this->is_required,
+            'created' => $this->created_at,
+            'updated' => $this->updated_at,
+        ];
+    }
+}
