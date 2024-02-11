5a2f22faf40db90bffd3f50787b3f984644a4f41
Saqib Razzaq <saqib.razzaq@plana.earth>
2024-02-11 16:20:28 +0100

    add duplicate field check

--- src/Services/FieldService.php
+++ src/Services/FieldService.php
@@ -2,6 +2,7 @@
 
 namespace Sakydev\Boring\Services;
 
+use Sakydev\Boring\Exceptions\BadRequestException;
 use Sakydev\Boring\Exceptions\NotFoundException;
 use Sakydev\Boring\Models\Field;
 use Sakydev\Boring\Repositories\CollectionRepository;
@@ -18,6 +19,11 @@ class FieldService
         $collectionDetails = $this->collectionRepository->getByName($collectionName);
         if (!$collectionDetails) {
             throw new NotFoundException('item.error.notFound');
+        }
+
+        $duplicateFieldExists = $this->fieldRepository->duplicateFieldExists($content['name'], $collectionDetails->id);
+        if ($duplicateFieldExists) {
+            throw new BadRequestException('item.error.alreadyExists');
         }
 
         return $this->fieldRepository->store($content, $collectionDetails->id);
