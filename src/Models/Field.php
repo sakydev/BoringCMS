b0a77144d4fdb5076632f89ba09abd9ecfd64682
Saqib Razzaq <saqib.razzaq@plana.earth>
2024-02-11 16:12:40 +0100

    update field model

--- src/Models/Field.php
+++ src/Models/Field.php
@@ -11,7 +11,27 @@ class Field extends Model
 {
     use HasFactory;
 
-    protected $protected = ['created_at', 'updated_at'];
+    protected $guarded = ['created_at', 'updated_at'];
+
+    public const TYPE_SHORT_TEXT = 'short_text';
+    public const TYPE_LONG_TEXT = 'long_text';
+    public const TYPE_MARKDOWN = 'markdown';
+    public const TYPE_RICHTEXT = 'richtext';
+    public const TYPE_FLOAT = 'float';
+    public const TYPE_INTEGER = 'integer';
+    public const TYPE_LIST = 'list';
+    public const TYPE_JSON = 'json';
+
+    public const SUPPORTED_TYPES = [
+        self::TYPE_SHORT_TEXT => 'string',
+        self::TYPE_LONG_TEXT => 'text',
+        self::TYPE_MARKDOWN => 'text',
+        self::TYPE_RICHTEXT => 'text',
+        self::TYPE_FLOAT => 'float',
+        self::TYPE_INTEGER => 'integer',
+        self::TYPE_LIST => 'array',
+        self::TYPE_JSON => 'json'
+    ];
 
     /**
      * Create a new factory instance for the model.
