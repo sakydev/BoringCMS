8066eb8da5cd76bd9499f9236dcee1d24001cedf
Saqib Razzaq <saqib.razzaq@plana.earth>
2024-02-11 10:53:21 +0100

    add field repo

---
+++ src/Repositories/FieldRepository.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Sakydev\Boring\Repositories;
+
+use Sakydev\Boring\Models\Field;
+
+class FieldRepository
+{
+    public function store(array $content, int $collectionId): Field {
+        $field = new Field();
+
+        $field->fill($content);
+        $field->collection_id = $collectionId;
+
+        $field->save();
+
+        return $field;
+    }
+}
