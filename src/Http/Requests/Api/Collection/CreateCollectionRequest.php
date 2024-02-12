a38aea97f02de28f3cb36208d99c911bd7070943
Saqib Razzaq <saqib.razzaq@plana.earth>
2024-02-12 03:23:04 +0100

    add CreateCollectionRequest

---
+++ src/Http/Requests/Api/Collection/CreateCollectionRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Sakydev\Boring\Http\Requests\Api\Collection;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use Sakydev\Boring\Models\Field;
+
+class CreateCollectionRequest extends FormRequest
+{
+    public function rules()
+    {
+        return [
+            'name' => [
+                'required',
+                'string',
+                'min:3',
+                'max:50',
+                'regex:/^[0-9a-z_-]+$/i',
+            ],
+            'is_hidden' => [
+                'sometimes',
+                'required',
+                'boolean',
+            ],
+            'description' => [
+                'sometimes',
+                'required',
+                'string',
+                'max:100',
+            ],
+        ];
+    }
+}
