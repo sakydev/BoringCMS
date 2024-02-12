ccdddf77e7145e2e0f1e300a502e2593902a204a
Saqib Razzaq <saqib.razzaq@plana.earth>
2024-02-12 11:50:03 +0100

    add anyNameExists and storeMany

--- src/Repositories/FieldRepository.php
+++ src/Repositories/FieldRepository.php
@@ -2,6 +2,8 @@
 
 namespace Sakydev\Boring\Repositories;
 
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Arr;
 use Sakydev\Boring\Models\Field;
 
 class FieldRepository
@@ -12,6 +14,14 @@ class FieldRepository
             ->where('collection_id', $collectionId)
             ->exists();
     }
+
+    public function anyNameExists(array $names, int $collectionId): bool {
+        return (new Field())
+            ->whereIn('name', $names)
+            ->where('collection_id', $collectionId)
+            ->exists();
+    }
+
     public function store(array $content, int $collectionId): Field {
         $field = new Field();
 
@@ -22,5 +32,28 @@ class FieldRepository
         $field->refresh();
 
         return $field;
+    }
+
+    public function storeMany(array $contents, int $collectionId): Collection
+    {
+        $fillables = (new Field())->getFillable();
+
+        $fields = collect();
+
+        foreach ($contents as $name => $content) {
+            $content['name'] = $name;
+            $content['collection_id'] = $collectionId;
+            if (!isset($content['is_required'])) {
+                $content['is_required'] = false;
+            }
+
+            $fields->push(Arr::only($content, $fillables));
+        }
+
+        Field::insert($fields->toArray());
+
+        return Field::where('collection_id', $collectionId)
+            ->whereIn('name', $fields->pluck('name'))
+            ->get();
     }
 }
