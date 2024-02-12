4e1456b1e310051be592713dd817c786597cef59
Saqib Razzaq <saqib.razzaq@plana.earth>
2024-02-12 03:22:07 +0100

    add collection controller

--- src/Http/Controllers/Api/Collection/CollectionController.php
+++ src/Http/Controllers/Api/Collection/CollectionController.php
@@ -2,28 +2,28 @@
 
 namespace Sakydev\Boring\Http\Controllers\Api\Collection;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Log;
 use Sakydev\Boring\Exceptions\BadRequestException;
-use Sakydev\Boring\Http\Requests\Api\Field\CreateFieldRequest;
-use Sakydev\Boring\Resources\Api\FieldResource;
+use Sakydev\Boring\Http\Requests\Api\Collection\Field\CreateFieldRequest;
+use Sakydev\Boring\Resources\Api\CollectionResource;
 use Sakydev\Boring\Resources\Api\Responses\BadRequestErrorResponse;
 use Sakydev\Boring\Resources\Api\Responses\ExceptionErrorResponse;
 use Sakydev\Boring\Resources\Api\Responses\SuccessResponse;
-use Sakydev\Boring\Services\FieldService;
-use Illuminate\Http\JsonResponse;
+use Sakydev\Boring\Services\CollectionService;
 use Symfony\Component\HttpFoundation\Response;
 use Throwable;
 
 class CollectionController
 {
-    public function __construct(readonly FieldService $fieldService) {}
+    public function __construct(readonly CollectionService $collectionService) {}
 
     public function store(CreateFieldRequest $createRequest, string $collectionName): JsonResponse {
         try {
-            $field = $this->fieldService->store($createRequest->validated(), $collectionName);
+            $field = $this->collectionService->store($createRequest->validated(), $collectionName);
 
             return new SuccessResponse('item.success.createOne', [
-                'field' => new FieldResource($field),
+                'field' => new CollectionResource($field),
             ], Response::HTTP_CREATED);
         } catch (BadRequestException $exception) {
             return new BadRequestErrorResponse($exception->getMessage());
