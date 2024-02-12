087cd77229047daf9c16a345566112aae302214f
Saqib Razzaq <saqib.razzaq@plana.earth>
2024-02-12 03:41:45 +0100

    set correct type for collection.is_hidden

--- migrations/2024_02_03_201629_create_collections_table.php
+++ migrations/2024_02_03_201629_create_collections_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('description')->nullable();
-            $table->string('is_hidden')->default(false);
+            $table->boolean('is_hidden')->default(false);
 
             $table->unsignedBigInteger('created_by');
             $table->unsignedBigInteger('updated_by')->nullable();
