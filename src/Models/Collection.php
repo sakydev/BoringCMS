042793706852316ce2f385d3625e1d4be649dcbe
Saqib Razzaq <saqib.razzaq@plana.earth>
2024-02-17 08:22:31 +0100

    add collections and fields relation

--- src/Models/Collection.php
+++ src/Models/Collection.php
@@ -5,6 +5,7 @@ namespace Sakydev\Boring\Models;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Sakydev\Boring\Database\Factories\CollectionFactory;
 
 class Collection extends Model
@@ -19,5 +20,9 @@ class Collection extends Model
     protected static function newFactory(): Factory
     {
         return CollectionFactory::new();
+    }
+
+    public function fields(): HasMany {
+        return $this->hasMany(Field::class, 'collection_id');
     }
 }
