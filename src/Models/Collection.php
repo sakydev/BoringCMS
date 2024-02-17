0b8069eea8bfab78902e72eb8597ae51d1907df1
Saqib Razzaq <saqib.razzaq@plana.earth>
2024-02-17 08:23:37 +0100

    automatically drop field entries related to collection

--- src/Models/Collection.php
+++ src/Models/Collection.php
@@ -25,4 +25,14 @@ class Collection extends Model
     public function fields(): HasMany {
         return $this->hasMany(Field::class, 'collection_id');
     }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        // Register the "deleting" event to delete related fields
+        static::deleting(function ($collection) {
+            $collection->fields()->delete();
+        });
+    }
 }
